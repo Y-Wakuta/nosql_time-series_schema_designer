2edeaa68527eddea588dedc7a4deac58f81c1996
Y-Wakuta <prsprivatestock@i.softbank.jp>
2019-09-26 13:26:26 -0400

    add test for time-depend search

---
+++ spec/time_depend_search_spec.rb
@@ -0,0 +1,66 @@
+require_relative './support/dummy_cost_model'
+
+module NoSE
+  module Search
+    describe Search do
+      include_context 'dummy cost model'
+      let(:timesteps) { 3 }
+
+      let(:td_workload) {
+        ts = timesteps
+        TimeDependWorkload.new do
+          TimeSteps ts
+
+          (Entity 'users' do
+            ID         'id'
+            String     'firstname', 6
+            String     'lastname', 7
+            String     'rating', 23
+          end) * 2_000
+
+          (Entity 'items' do
+            ID         'id'
+            String     'name', 19
+            String     'description', 197
+            Integer    'quantity', count: 100
+          end) * 20_000
+
+          Group 'Test1', 0.5, :increase do
+            Q 'SELECT users.* FROM users WHERE users.id = ? -- 0', [0.01, 0.5, 9]
+            Q 'SELECT items.* FROM items WHERE items.id=? -- 2', [9, 0.5, 0.01]
+          end
+        end
+      }
+
+      it 'correct number of timesteps in output' do
+        indexes = IndexEnumerator.new(td_workload).indexes_for_workload.to_a
+        result = Search.new(td_workload, cost_model).search_overlap indexes
+        expect(result.indexes.size).to eq timesteps # check for indexes
+        expect(result.plans.map{|plan| plan.size}.uniq.first).to eq timesteps # check for plans
+      end
+
+      it 'the query plan changes when the frequency changes' do
+        query_increase = 'SELECT users.* FROM users WHERE users.rating=? -- 1'
+        query_decrease = 'SELECT items.* FROM items WHERE items.quantity=? -- 3'
+        td_workload.add_statement query_increase, [0.01, 0.5, 9]
+        td_workload.add_statement query_decrease, [9, 0.5, 0.01]
+        indexes = IndexEnumerator.new(td_workload).indexes_for_workload.to_a
+        result = Search.new(td_workload, cost_model).search_overlap indexes, 9800000
+
+        increase_steps = result.plans.select{|plan_all| plan_all.first.query.text == query_increase}.flatten(1)
+        decrease_steps = result.plans.select{|plan_all| plan_all.first.query.text == query_decrease}.flatten(1)
+
+        expect(increase_steps.first.steps.size).to be >  increase_steps.last.steps.size
+        expect(decrease_steps.first.steps.size).to be <  decrease_steps.last.steps.size
+      end
+
+      it 'deal with query that has sort step' do
+        td_workload.add_statement 'SELECT users.* FROM users WHERE users.rating = ? ORDER BY users.lastname -- 4', [0.01, 0.5, 9]
+        indexes = IndexEnumerator.new(td_workload).indexes_for_workload.to_a
+        result = Search.new(td_workload, cost_model).search_overlap indexes
+        expect(result.indexes.size).to eq timesteps # check for indexes
+        expect(result.plans.map{|plan| plan.size}.uniq.first).to eq timesteps # check for plans
+      end
+    end
+  end
+end
--- workloads/time_depend/minimum.rb
+++ workloads/time_depend/minimum.rb
@@ -9,20 +9,19 @@ NoSE::TimeDependWorkload.new do
   DefaultMix :browsing
   TimeSteps 3
 
-  Group 'UsersInfo', 1.0, :decrease, browsing: 4.41,
+  Group 'UsersInfo', 1.0, browsing: 4.41,
         bidding: 2.48,
         write_medium: 2.48,
         write_heavy: 2.48 do
-    Q 'SELECT users.* FROM users WHERE users.id = ? -- 8', [0.9, 0.5, 0.01]
-    Q 'SELECT users.* FROM users WHERE users.rating=? -- 12', [0.9, 0.5, 0.01]
-   # Q 'SELECT bids.* FROM items.bids WHERE items.id = ? -- 6', [0.01, 0.8, 1.4]
+    Q 'SELECT users.* FROM users WHERE users.id = ? -- 8', [0.1, 0.5, 0.9]
+    Q 'SELECT users.* FROM users WHERE users.rating =? -- 12', [1, 5, 9]
   end
 
-  Group 'ItemsInfo', 1.0, :increase, browsing: 8.82,
+  Group 'ItemsInfo', 1.0, browsing: 8.82,
         bidding: 5.96,
         write_medium: 4.96,
         write_heavy: 4.96 do
-    Q 'SELECT items.* FROM items WHERE items.id=? -- 13', [0.01, 0.5, 0.9]
-    Q 'SELECT items.* FROM items WHERE items.quantity=? -- 13', [0.01, 0.5, 0.9]
+    Q 'SELECT items.* FROM items WHERE items.id=? -- 13', [0.9, 0.5, 0.1]
+    Q 'SELECT items.* FROM items WHERE items.description=? -- 13 LIMIT 1', [0.9, 0.5, 0.1]
   end
 end
