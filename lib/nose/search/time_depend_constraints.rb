52e20d2868eff762a3eb01c2fdb2941ce1aab313
Y-Wakuta <prsprivatestock@i.softbank.jp>
2020-01-19 00:50:35 +0900

    #2 update TimeDependPrepareConstraint to new implementation

--- lib/nose/search/time_depend_constraints.rb
+++ lib/nose/search/time_depend_constraints.rb
@@ -47,25 +47,6 @@ module NoSE
       end
     end
 
-    class TimeDependPrepareConstraints < Constraint
-      def self.apply(problem)
-        l = problem.index_vars.size.to_f
-        problem.trees.each do |tree|
-          tree.each do |plan|
-            (1...problem.timesteps).each do |ts|
-              prepare_var = problem.prepare_vars[tree.query].find{|key, _| key == plan}.last[ts]
-
-              lookup_steps = plan.select{|step| step.is_a? Plans::IndexLookupPlanStep}
-              current_plan = lookup_steps.reduce(MIPPeR::LinExpr) {|_, step| problem.index_vars[step.index][ts] * 1.0}
-              former_plan = lookup_steps.reduce(MIPPeR::LinExpr){|_, step| problem.index_vars[step.index][ts - 1] * -1.0}
-
-              constr = MIPPeR::Constraint.new(prepare_var * l + current_plan + former_plan, :>=, 0, name)
-              problem.model << constr
-            end
-          end
-        end
-      end
-    end
 
     # The single constraint used to enforce a maximum storage cost
     class TimeDependSpaceConstraint < Constraint
@@ -135,6 +116,8 @@ module NoSE
 
       # Add complete query plan constraints
       def self.apply_query(query, q, problem)
+        return if query.is_a? MigrateSupportQuery
+
         entities = query.join_order
         query_constraints_whole_time = (0...problem.timesteps).map do |_|
           Hash[entities.each_cons(2).map do |e, next_e|
@@ -152,6 +135,65 @@ module NoSE
             parent_index = steps.first.parent.parent_index
             next if parent_index.nil?
             parent_var = problem.query_vars[parent_index][query][ts]
+            ensure_parent_index_available(index, index_var, parent_var, problem, q)
+          end
+
+          # Ensure we have exactly one index on each component of the query graph
+          add_query_constraints query, q, query_constraints, problem, ts
+        end
+      end
+    end
+
+    class TimeDependPrepareConstraints < CompletePlanConstraints
+      def self.add_query_constraints(query, q, constraints, problem, timestep)
+        constraints.each do |entities, constraint|
+          name = "q#{q}_#{entities.map(&:name).join '_'}_#{timestep}" \
+              if ENV['NOSE_LOG'] == 'debug'
+
+          # the requirement of a plan conditional on this index
+          index_var = if problem.data[:by_id_graph]
+                        problem.index_vars[query.index.to_id_graph][timestep]
+                      else
+                        problem.index_vars[query.index]&.fetch(timestep)
+                      end
+
+          # if index_var is nil, the index is not used in any query plan
+          next if index_var.nil?
+
+          if timestep < (problem.timesteps - 1) # The SupportQuery maybe for updating migrate-preparing indexes
+            next_timestep_migrate_var = problem.migrate_vars[query.index]&.fetch(timestep + 1)
+
+            fail "if index_var exists, next_timestep_migrate_var also should exists" if next_timestep_migrate_var.nil?
+
+            # TODO: even if index_var and next_timestep_migrate_var are 0, constraint can take 1
+            constr_next_timestep= MIPPeR::Constraint.new constraint + next_timestep_migrate_var * -1.0,
+                                                         :==, 0, name
+            problem.model << constr_next_timestep
+          end
+        end
+      end
+
+      # Add complete query plan constraints
+      def self.apply_query(query, q, problem)
+        return unless query.is_a? MigrateSupportQuery
+
+        entities = query.join_order
+        query_constraints_whole_time = (0...(problem.timesteps - 1)).map do |_|
+          Hash[entities.each_cons(2).map do |e, next_e|
+            [[e, next_e], MIPPeR::LinExpr.new]
+          end]
+        end
+
+        query_constraints_whole_time.each_with_index do |query_constraints, ts|
+          first, last = setup_query_constraints(query_constraints, entities)
+
+          problem.data[:costs][query].each do |index, (steps, _)|
+            index_var = problem.prepare_vars[index][query][ts]
+            construct_query_constraints(index, index_var, steps, entities, query_constraints, first, last)
+
+            parent_index = steps.first.parent.parent_index
+            next if parent_index.nil?
+            parent_var = problem.prepare_vars[parent_index][query][ts]
             ensure_parent_index_available(index, index_var, parent_var, problem, q)
           end
 
