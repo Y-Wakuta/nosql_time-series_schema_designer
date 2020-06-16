3964dd5524dfa378196517a86ecc52d55318beda
Y-Wakuta <prsprivatestock@i.softbank.jp>
2020-06-17 02:22:24 +0900

    32 enumerate eq_choices for small subgraphs

--- lib/nose/pruned_enumerator.rb
+++ lib/nose/pruned_enumerator.rb
@@ -19,10 +19,10 @@ module NoSE
       puts "entity_fields (pattern) size: " + entity_fields.size.to_s
       extra_fields = get_frequent_extra_choices queries
 
-      #indexes = Parallel.flat_map(queries, in_processes: 12) do |query|
-      indexes = queries.flat_map do |query|
+      indexes = Parallel.flat_map(queries, in_processes: 12) do |query|
+      #indexes = queries.flat_map do |query|
         idxs = indexes_for_query(query, entity_fields[query], extra_fields).to_a
-        used_idxes = get_used_indexes_in_query_plans [query], idxs
+        get_used_indexes_in_query_plans [query], idxs
         idxs
       end
       indexes += additional_indexes
@@ -61,8 +61,7 @@ module NoSE
 
     # Since support queries are simple, use IndexEnumerator to make migration easier
     def support_indexes(indexes, by_id_graph)
-      index_enumerator = IndexEnumerator.new(@workload)
-      return index_enumerator.support_indexes indexes, by_id_graph
+      IndexEnumerator.new(@workload).support_indexes indexes, by_id_graph
     end
 
     private
@@ -84,7 +83,7 @@ module NoSE
         fields.permutation.to_a
       end.uniq << []
       extra_choices = frequent_extra_choices(graph, select, eq, range, extra_fields)
-      #puts("graph : #{graph.inspect}")
+      puts("graph : #{graph.entities.map{|n| n.name}}")
       puts("eq_choices : #{eq_choices.size}")
       puts("extra_choices : #{extra_choices.size}")
 
@@ -142,12 +141,17 @@ module NoSE
       FpGrowth.mine(extras).select{|ef| ef.support > 1}
     end
 
-    #候補数の母数を減らして，列挙数を増やせばより効率的にできるはず．列挙についてに，並列処理を入れる
     def frequent_eq_choices(graph, group_by, entity_fields_patterns)
       # filter not used fields for this sub-graph
-      #entity_fields_patterns[:shared] = entity_fields_patterns[:shared].map{|efps| efps.select{|efs| efs.all?{|ef| graph.entities.include? ef.parent}}}
       shared_patterns = entity_fields_patterns[:shared].select{|efs| efs.all?{|ef| graph.entities.include? ef.parent}}
       unique_patterns = entity_fields_patterns[:unique].select{|efpu| efpu.all?{|efp| graph.entities.include? efp.parent}}
+
+      # frequent entity_choies are combined. However, this result in no eq_choices for smapp subgraphs
+      if shared_patterns.empty?
+        shared_patterns = entity_fields_patterns[:shared]
+                              .select{|efs| efs.map(&:parent).uniq.size > 1}
+                              .map{|efs| efs.select{|f| graph.entities.include? f.parent}}
+      end
 
       # get each enumeration combination size for shared fields and unique fields
       graph_entities_size_for_shared = shared_patterns.flatten.map(&:parent).uniq.size
@@ -162,26 +166,18 @@ module NoSE
       end << []
 
       # TODO: tmp
-      unique_eq_choices = unique_eq_choices.map{|uec| sort_by_is_primary uec}
+      unique_eq_choices = unique_eq_choices.flat_map{|uec| enumerate_by_primary_fields uec}
 
-      # no need for enumerate for all entities because we add materialized view
       eq_choices = 1.upto(graph_entities_size_for_shared).flat_map do |n|
-        #eq_choices = 1.upto(graph_entities_size_for_shared - 1).flat_map do |n|
-
-        #shareds = entity_fields_patterns[:shared].flat_map{|efp| efp.permutation(n).to_a}
-        shareds = shared_patterns.permutation(n).to_a
-        # ここで候補を出し過ぎなのは間違いないが，ジョインプランが出るようにはしたい. share しているクエリ間で同じ分割方法をしていれば問題ないはず？ソートして partition をとるか
-        #shareds = entity_fields_patterns[:shared].map{|efp| efp.map(&:to_set).uniq.map(&:to_a).partitions(n).to_a}.flatten(2).reject{|e| e.empty?}
-        #shareds = entity_fields_patterns[:shared].map{|efp| efp.map(&:to_set).uniq.map(&:to_a).combination(n).map(&:flatten).map(&:uniq).uniq}
-        #shareds = entity_fields_patterns[:shared].map{|efp| efp.map(&:to_set).uniq.map(&:to_a).combination(n).map{|rss| rss.sort_by { |rs | rs.map{|r| r.name}.uniq.join }}.map(&:flatten).map(&:uniq).uniq}
+        #shareds = shared_patterns.combination(n).flat_map{|sp| enumerate_by_primary_fields sp.flatten(1)} << []
+        shareds = shared_patterns.permutation(n).to_a << []
         (shareds.product(unique_eq_choices) + unique_eq_choices.product(shareds)).map{|f| f.flatten.uniq} + shareds.map(&:flatten)
       end + unique_eq_choices
 
-      tmp = 1.upto(3).flat_map do |n|
+      eq_choices += 1.upto(2).flat_map do |n|
         additional_eqs = (shared_patterns + unique_patterns).permutation(n).to_a.map(&:flatten).map(&:uniq).uniq
-        additional_eqs.map{|aeps| sort_by_not_primary aeps}
-      end
-      eq_choices += tmp.uniq
+        additional_eqs.flat_map{|aeps| enumerate_by_non_primary_fields aeps}
+      end.uniq
 
       #unless group_by.empty?
       #  eq_choices = prune_eq_choices_for_groupby eq_choices, group_by
@@ -209,22 +205,12 @@ module NoSE
     # TODO: At least for extra_fields, reduce for all patterns reduces candidates too much. I need to used only effective sets
     # group choices if the choices are included in the same frequent pattern
     def reduce_choices(entity_choice, patterns, threshold)
-      #patterns.select{|p| p.support >= threshold}.sort_by { |p| [-p.content.size, p.support]}.map(&:content).each do |pattern|
-      #  if entity_choice.to_set >= pattern.to_set
-      #    # ここで，一度実行すると配列のネストが深くなるので，二度目以降は実行されなくなる
-      #    entity_choice = (entity_choice - pattern).map{|f| [f]} + [pattern]
-      #  end
-      #end
       frequent_fields_set = patterns.select{|p| p.support >= threshold}
                                 .sort_by { |p| [-p.content.size, p.support]}
                                 .map(&:content)
                                 .first
-      #return [entity_choice] if frequent_fields_set.nil?
-      #entity_choice = (entity_choice - frequent_fields_set).map{|f| [f]} + [sort_by_is_primary(frequent_fields_set.flatten.uniq)]
-
       return entity_choice if frequent_fields_set.nil?
-      entity_choice = (entity_choice - frequent_fields_set) + [sort_by_is_primary(frequent_fields_set.flatten.uniq)]
-
+      entity_choice = (entity_choice - frequent_fields_set) + [frequent_fields_set.flatten.uniq]
       entity_choice
     end
 
@@ -247,30 +233,16 @@ module NoSE
       (a.size % 2).zero? ? a[a.size/2 - 1, 2].inject(:+) / 2.0 : a[a.size/2]
     end
 
-    def sort_by_is_primary(fields)
-      primary_fields = fields.select{|f| f.primary_key?}.sort_by { |fp| [-fp.cardinality, fp.name]}
+    def enumerate_by_primary_fields(fields)
+      primary_fields = fields.select{|f| f.primary_key?}.permutation.to_a
       non_primary_fields = fields.select{|f| !f.primary_key?}.sort_by { |fp| [-fp.cardinality, fp.name]}
-      primary_fields + non_primary_fields
+      primary_fields.product(non_primary_fields).map(&:flatten)
     end
 
-    def sort_by_not_primary(fields)
+    def enumerate_by_non_primary_fields(fields)
       primary_fields = fields.select{|f| f.primary_key?}.sort_by { |fp| [-fp.cardinality, fp.name]}
-      non_primary_fields = fields.select{|f| !f.primary_key?}.sort_by { |fp| [-fp.cardinality, fp.name]}
-      non_primary_fields + primary_fields
-    end
-
-    def entity_field_combinations(entity_fields)
-      return [] if entity_fields.size == 0
-      entity_hash = {}
-      entity_fields.reject{|efs| efs.empty?}.each do |efs|
-        fail 'each entity fields should have only one entity' if efs.map(&:parent).uniq.size > 1
-        unless entity_hash.has_key? efs.first.parent
-          entity_hash[efs.first.parent] = []
-          entity_hash[efs.first.parent] << []
-        end
-        entity_hash[efs.first.parent] << efs
-      end
-      entity_hash.values[0].product(*entity_hash.values[1..-1])
+      non_primary_fields = fields.select{|f| !f.primary_key?}.permutation.to_a
+      non_primary_fields.product(primary_fields).map(&:flatten)
     end
   end
 end
