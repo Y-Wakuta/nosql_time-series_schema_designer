6260479a3fbaf5f6773ca5f02e6733910e52db09
Y-Wakuta <prsprivatestock@i.softbank.jp>
2021-02-25 18:23:57 +0900

    \#61 fixed the bug that added constraint to start ts and middle ts

--- lib/nose/search/time_depend_iterative_constraints.rb
+++ lib/nose/search/time_depend_iterative_constraints.rb
@@ -6,11 +6,13 @@ module NoSE
       def self.apply(problem)
         return unless (problem.ts_indexes.has_key?(problem.start_ts) and problem.ts_indexes.has_key?(problem.end_ts))
 
-        tss = [problem.start_ts, problem.end_ts]
-        tss.each do |ts|
+        target_tss = [problem.start_ts, problem.middle_ts, problem.end_ts]
+        fixed_tss = [problem.start_ts, problem.end_ts]
+        fixed_tss.each do |ts|
           problem.ts_indexes[ts].each do |idx|
             name = "#{idx.key}_fixed_#{ts}" if ENV['NOSE_LOG'] == 'debug'
-            constr = MIPPeR::Constraint.new problem.index_vars[idx][tss.index(ts)] * 1.0,
+            puts "force #{idx.key} to be 1 at #{ts} of #{target_tss}"
+            constr = MIPPeR::Constraint.new problem.index_vars[idx][target_tss.index(ts)] * 1.0,
                                             :==, 1, name
             problem.model << constr
           end
