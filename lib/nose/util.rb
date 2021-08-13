120f5b7cda9b0ecbddb75483459c1f521bc45853
Y-Wakuta <yusuke.wakuta@gmail.com>
2021-08-14 02:57:03 +0900

    change selectivity reduction ratio by range condition

--- lib/nose/util.rb
+++ lib/nose/util.rb
@@ -169,7 +169,7 @@ class Cardinality
   # Update the cardinality based on filtering implicit to the index
   # @return [Fixnum]
   def self.filter(cardinality, eq_filter, range_filter)
-    range_selectivity = 3.0
+    range_selectivity = 2.0
     filtered = (range_filter.nil? ? 1.0 : (1.0 / range_selectivity)) * cardinality
     filtered *= eq_filter.map do |field|
       1.0 / field.cardinality
