bdeb59296978236ed3e9f385c9d32acdb2d81077
Y-Wakuta <yusuke.wakuta@gmail.com>
2022-02-20 01:24:42 +0900

    use calibrated value for calculating reduction rate for each filtering condition

--- lib/nose/util.rb
+++ lib/nose/util.rb
@@ -174,7 +174,7 @@ class Cardinality
   # Update the cardinality based on filtering implicit to the index
   # @return [Fixnum]
   def self.filter(cardinality, eq_filter, range_filter)
-    range_selectivity = 10.0
+    range_selectivity = 1.0 / 0.504190380952381
     filtered = (range_filter.empty? ? 1.0 : (1.0 / range_selectivity) ** range_filter.size) * cardinality
     filtered *= eq_filter.map do |field|
       1.0 / field.cardinality
